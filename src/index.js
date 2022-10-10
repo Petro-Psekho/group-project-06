e004260fbb97e5884b2a3091963e67af2fc28f30
Kiterinka <misangelais@gmail.com>
2022-10-10 11:34:59 +0300

    js, svg

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './js/modal-chocolate-icecream';
 import './js/modal-locations';
-// import './js/modal-how-its-made';
+import './js/modal-how-its-made';
 import './js/modal-franchise';
 import './js/slider-reviews';
-
 // import './js/wow';
 // new WOW().init();
 
