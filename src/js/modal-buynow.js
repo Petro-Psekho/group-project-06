e930f859f23257e94378be1b49251c02714b6086
DimaHavr <109313387+DimaHavr@users.noreply.github.com>
2022-10-11 19:18:42 +0300

    update modal-bynow.js

--- src/js/modal-buynow.js
+++ src/js/modal-buynow.js
@@ -1,14 +1,17 @@
 (() => {
   const buy = {
-    openModalBtn: document.querySelector("[data-modal-open-buy-now]"),
-    closeModalBtn: document.querySelector("[data-modal-close-buy-now]"),
-    modal: document.querySelector("[data-modal-buy-now]"),
+    openModalBtn: document.querySelector('[data-modal-open-buy-now]'),
+    openMenuModalBtn: document.querySelector('[data-menu-modal-open-buy-now]'),
+    closeModalBtn: document.querySelector('[data-modal-close-buy-now]'),
+    modal: document.querySelector('[data-modal-buy-now]'),
   };
 
-  buy.openModalBtn.addEventListener("click", toggleModal);
-  buy.closeModalBtn.addEventListener("click", toggleModal);
+  buy.openModalBtn.addEventListener('click', toggleModal);
+  buy.closeModalBtn.addEventListener('click', toggleModal);
+  buy.openMenuModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    buy.modal.classList.toggle("is-hidden");
+    buy.modal.classList.toggle('is-hidden');
+    buy.body.classList.toggle('no-scroll');
   }
 })();
