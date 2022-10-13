68bee810f85511fcd0db29da3c6e16f0bceea0e1
Petro Psekho <psekho.pp@gmail.com>
2022-10-13 15:29:21 +0300

    adds scroll header

---
+++ src/js/scroll.js
@@ -0,0 +1,17 @@
+let scrollpos = window.scrollY;
+
+const header = document.querySelector('header');
+const scrollChange = 1;
+
+const add_class_on_scroll = () => header.classList.add('bg');
+const remove_class_on_scroll = () => header.classList.remove('bg');
+
+window.addEventListener('scroll', function () {
+  scrollpos = window.scrollY;
+
+  if (scrollpos >= scrollChange) {
+    add_class_on_scroll();
+  } else {
+    remove_class_on_scroll();
+  }
+});
